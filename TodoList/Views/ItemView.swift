1f4780c66215b329d908f361a054acbf4481d8ac
Ryan S <ryan@Ryans-Air.socal.rr.com>
2024-07-14 12:51:08 -0700

    added itemview and functionality to plus button
    test commit

---
+++ TodoList/Views/ItemView.swift
@@ -0,0 +1,18 @@
+//
+//  ItemView.swift
+//  TodoList
+//
+//  Created by Ryan S on 7/14/24.
+//
+
+import SwiftUI
+
+struct ItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ItemView()
+}
